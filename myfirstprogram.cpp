3baef4277a85d1a674c945429cbbd77efa441471
Solehin Sulaiman <104151227+choinsyn@users.noreply.github.com>
2023-12-27 02:40:18 +0800

    Add files via upload

---
+++ myfirstprogram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  string name = "Solehin" ;
+  int age = 27 ;
+
+  cout << "Hello World!" << endl;
+  cout << "Im " << name << ", " << age << " years old" << endl;
+  
+
+
+  return 0;
+}
