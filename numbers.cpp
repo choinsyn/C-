b82120e8579823ee34ba2146add8a4e8b8d444fc
Solehin Sulaiman <104151227+choinsyn@users.noreply.github.com>
2023-12-27 02:42:37 +0800

    Add files via upload

---
+++ numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std; 
+
+int main()
+{
+    int number = 5;
+    
+
+    cout << pow(number, 2) ;
+
+
+    return 0;
+}
---
+++ variableWithFunction.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Solehin Sulaiman";
+
+    cout << name.substr(8 , 5);
+
+
+
+    return 0;
+}
