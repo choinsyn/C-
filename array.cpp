5ebd082504484aced1240a71bb580631a097df44
Solehin Sulaiman <104151227+choinsyn@users.noreply.github.com>
2023-12-29 00:33:23 +0800

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name[] = {"solehin" , "muhammad" , "nur" };
+    int age[] = {27 , 28, 29};
+
+    cout << "His name is " << name[2] << endl;
+    cout << "He is " << age[2] << " years old" << endl;
+
+
+    return 0;
+}
