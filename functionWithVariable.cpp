8fd5cd4e2150df67f1ad15d2b84bd30c7f1786d8
Solehin Sulaiman <104151227+choinsyn@users.noreply.github.com>
2023-12-30 01:13:44 +0800

    Add files via upload

---
+++ functionWithVariable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void information(string name, int age)
+{
+    cout << "His name is " << name << " and he is " << age << " years old" ;
+}
+
+int main()
+{
+    information("Solehin", 27);
+
+
+    return 0;
+}
